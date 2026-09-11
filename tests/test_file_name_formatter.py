2fa1be33fcf1a8ef60c969b728639a132e3e4e39
mike galyen <mkgalyen@gmail.com>
2026-09-11 08:56:35 -0400

    add docstring to test file

--- tests/test_file_name_formatter.py
+++ tests/test_file_name_formatter.py
@@ -2,6 +2,10 @@ from file_name_formatter import _print_all_objects
 
 from pathlib import Path
 
+"""
+Unit tests for file_name_formatter.py tool
+"""
+
 def test_print_all_objects():
     path = Path(".")
     _print_all_objects(path)
